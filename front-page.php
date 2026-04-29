6dde8c9c9ac30d44bd6964b65b172d4159ea94a8
Andre Gutierrez <andregabriel.gg@gmail.com>
2026-04-29 14:42:43 -0400

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -11,7 +11,7 @@
                         <div class="badge_text">Get Recommended • Get Chosen • Get Clients Back</div>
                     </div>
                     <div class="spacer-xsmall"></div>
-                    <div class="max-heading is-42rem">
+                    <div class="max-heading is-54rem">
                         <h1 animation="heading">A Marketing Agency<br>Focused on Getting<br>Your Business in <em animation="growth">AI Answers.</em></h1>
                     </div>
                     <div class="max-description is-32rem">
