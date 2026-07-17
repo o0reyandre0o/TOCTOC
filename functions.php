adaaeff8b4e467e6169bc7c70332d669576c821b
Andre Gutierrez <andregabriel.gg@gmail.com>
2026-07-16 21:58:44 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -100,6 +100,7 @@ add_action( 'init', function () {
         [ 'https://toctoc.ky/social-media-marketing-services-cayman-islands/',     '0.8' ],
         [ 'https://toctoc.ky/web-development-cayman-islands/',                      '0.8' ],
         [ 'https://toctoc.ky/advertising-pr-agency-cayman-islands/',               '0.8' ],
+        [ 'https://toctoc.ky/our-work/',                                           '0.9' ],
         [ 'https://toctoc.ky/about-toc-toc-marketing/',                            '0.7' ],
         [ 'https://toctoc.ky/venezuela/',                                          '0.9' ],
         [ 'https://toctoc.ky/seo-checker/',                                        '0.7' ],
