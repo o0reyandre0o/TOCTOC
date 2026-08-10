25c74540f79890ff64782732aa0d050905a926d6
Andre Gutierrez <andregabriel.gg@gmail.com>
2026-08-10 10:08:49 -0400

    Update page-web-development-cayman-islands.php

--- page-web-development-cayman-islands.php
+++ page-web-development-cayman-islands.php
@@ -126,7 +126,7 @@ get_header(); ?>
             </p>
             <div class="mt-10">
                 <a href="<?php echo esc_url( home_url( '/website-design-agency-cayman-islands/' ) ); ?>" class="inline-flex items-center gap-2 font-bold text-sky-deep hover:gap-4 transition-all decoration-none">
-                    Explore Web Design <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M5 12h14"/><path d="m12 5 7 7-7 7"/></svg>
+                    Explore Website Development <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M5 12h14"/><path d="m12 5 7 7-7 7"/></svg>
                 </a>
             </div>
         </div>
